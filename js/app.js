ef0b5d9d4b9d2646f8d6442f6c3b8322f29e98cf
marinaadlc <marinaadlc@gmail.com>
2020-11-26 23:54:27 +0100

    header fixed

--- js/app.js
+++ js/app.js
@@ -1,24 +1,14 @@
 var burger = document.querySelector("#nav-burger-button");
-var nav = document.querySelector(".nav-bar");
+var nav = document.querySelector("#phone-nav .nav-bar");
 var burgerIcon = document.querySelector("#burger-icon");
 var closeIcon = document.querySelector("#close-icon");
+var body = document.querySelector("body");
 
-queryHandler();
-window.onresize=queryHandler;
-function queryHandler(){
-    if (window.matchMedia("(min-width: 700px)").matches){
-        nav.classList.remove('hidden');
-        nav.classList.remove('nav-phone-view');
-    }else{
-        nav.classList.add('hidden');
-        nav.classList.add('nav-phone-view');
-        burgerIcon.classList.remove('hidden');
-        closeIcon.classList.add('hidden');
-    }
-}
-
-burger.addEventListener('click',function(){
-    nav.classList.toggle('hidden');
+function menuToggle(){
+    nav.classList.toggle('hide-up');
     burgerIcon.classList.toggle('hidden');
     closeIcon.classList.toggle('hidden');
-});
+    body.classList.toggle('fixed');
+};
+
+burger.addEventListener('click',menuToggle);
