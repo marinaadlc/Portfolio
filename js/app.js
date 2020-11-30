355b08a783889c42ef61a9fa342bcca2ec16ea6d
marinaadlc <marinaadlc@gmail.com>
2020-11-30 17:20:50 +0100

    solucionado error en el menu de hamburguesa

--- js/app.js
+++ js/app.js
@@ -3,13 +3,9 @@ var nav = document.querySelector(".phone-nav .nav-bar");
 var burgerIcon = document.querySelector(".burger-icon");
 var closeIcon = document.querySelector(".close-icon");
 var body = document.querySelector("body");
-var customSel = document.querySelector(".custom-select");
-
-
 
 /* Mobile menu toggle */
 function menuToggle(){
-    customSel.classList.toggle("hidden");
     nav.classList.toggle('hide-up');
     burgerIcon.classList.toggle('hidden');
     closeIcon.classList.toggle('hidden');
@@ -36,46 +32,3 @@ function filterSelection(filter){
    }
  }
 }
-
-
-
-
-
-/* filterSelection("todos")
-function filterSelection(filter){
-    var x,i;
-    x = document.getElementsByClassName("works-select");
-    if (filter=="todos")filter="";
-    for(i=0; i<x.length; i++){
-        w3RemoveClass(x[i],"show");
-        if (x[i].className.indexOf(filter) > -1) w3AddClass(x[i], "show");
-    }   
-}
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-    }
-  }  
-function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);     
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-var customSelector = document.querySelector(".custom-select");
-var selectors = customSelector.getElementsByClassName("works-select");
-for (var i = 0; i < selectors.length; i++) {
-    selectors[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-} */
